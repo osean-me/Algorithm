03b5355f476842edb56009e44d24d456999cdb5c
SeongHeon Sim <deep@osean.me>
2023-06-02 18:19:08 +0900

    Day 33 : 백준 1620 문제 Java 로 풀기 성공

--- java_algorithm/Baekjoon_1620.java
+++ java_algorithm/Baekjoon_1620.java
@@ -2,17 +2,33 @@ package java_algorithm;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Baekjoon_1620 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static List<String> pokemons = new ArrayList<>();
-    private static List<String> result = new ArrayList<>();
+    private static Map<String, String> pokemonMap = new HashMap<>();
 
     private static void inputPokemons(int count) throws IOException {
         for (int i = 0; i < count; i++) {
-            pokemons.add(br.readLine());
+            String pokemon = br.readLine();
+            pokemons.add(pokemon);
+            pokemonMap.put(pokemon, String.valueOf(i + 1));
+        }
+    }
+
+    private static void findPokemons(int count) throws IOException {
+        for (int i = 0; i < count; i++) {
+            String input = br.readLine();
+            String result = pokemonMap.get(input);
+            if (result == null) {
+                result = pokemons.get(Integer.parseInt(input) - 1);
+            }
+            bw.write(result);
+            bw.newLine();
         }
     }
 
@@ -21,10 +37,13 @@ public class Baekjoon_1620 {
         int pokemonCount = Integer.parseInt(input[0]);
         int t = Integer.parseInt(input[1]);
         inputPokemons(pokemonCount);
+        findPokemons(t);
     }
 
 
     public static void main(String[] args) throws IOException {
         solution();
+        br.close();
+        bw.close();
     }
 }
