491e33280b2edeb1799911da0061cf4b3cb7d22f
SeongHeon Sim <deep@osean.me>
2024-11-27 00:12:35 +0900

    chore: [1주차] bj1629 노트 추가

--- cpp_algorithm/new_week_1/bj1629.cpp
+++ cpp_algorithm/new_week_1/bj1629.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 
 int go(long long a, long long b, long long c) {
+    // 모듈러 연산의 마지막 조건
     if (b == 1) return a % c;
 
+    // 제곱근의 절반씩 줄여나간다.
     long long tmp = go(a, b / 2, c);
-    tmp = tmp * tmp % c;
+    // 모듈러 연산 곱셈 기질
+    // (a * b) % c = (a % c) * (b * c) % c
+    // 위 공식에서 (a % c) 혹은 (b * c) 는 재귀함수의 기저사례!
+    tmp = (tmp * tmp) % c;
+    // 제곱근의 값이 홀수인 경우
+    // a 를 한 번 더 곱하도록 한다. ex) 2^3 = 2^2 * 2
     if (b % 2) tmp = (tmp * a) % c;
     return tmp;
 }
