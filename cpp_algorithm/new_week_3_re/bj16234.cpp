f571af705cffe5756f3b7022b4ea3c4740f778cf
SeongHeon Sim <deep@osean.me>
2025-05-22 21:42:04 +0900

    add: [3주차] bj16234 복습

--- cpp_algorithm/new_week_3_re/bj16234.cpp
+++ cpp_algorithm/new_week_3_re/bj16234.cpp
@@ -2,83 +2,82 @@
 
 using namespace std;
 
-int n, l, r, cnt;
+int n, l, r, ret;
 int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
-int board[50][50], visited[50][50];
+int board[50][50], flag[50][50];
+vector<vector<pair<int, int>>> visited;
 queue<pair<int, int>> points;
 
-bool validate() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (visited[i][j]) return true;
-        }
-    }
-    return false;
+bool check() { return visited.size() < (n * n); }
+
+bool validate(int gap) {
+    if (gap < 0) gap *= -1;
+    return l <= gap && gap <= r;
 }
 
-void check(int y, int x) {
-    for (int i = 0; i < 4; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
+void clear() {
+    visited.clear();
+    memset(flag, 0, sizeof(flag));
+}
 
-        if (ny < 0 || n <= ny || nx < 0 || n <= nx) continue;
-        if (visited[ny][nx]) continue;
+void go(int y, int x) {
+    flag[y][x] = 1;
+    (*(visited.end() - 1)).push_back({y, x});
+    points.push({y, x});
 
-        int ret = board[y][x] - board[ny][nx];
-        if (ret < 0) ret *= -1;
-        if (ret < l || r < ret) continue;
+    while (points.size()) {
+        int cy, cx;
+        tie(cy, cx) = points.front();
+        points.pop();
 
-        visited[ny][nx] = 1;
-        check(ny, nx);
+        for (int i = 0; i < 4; i++) {
+            int ny = cy + dy[i];
+            int nx = cx + dx[i];
+            if (ny < 0 || n <= ny || nx < 0 || n <= nx) continue;
+            if (flag[ny][nx]) continue;
+            if (!validate(board[cy][cx] - board[ny][nx])) continue;
+
+            flag[ny][nx] = flag[cy][cx] + 1;
+            go(ny, nx);
+            points.push({ny, nx});
+        }
     }
 }
 
-void move() {
-    int c = 0;
-    int ret = 0;
-    visited[0][0] = 1;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (!visited[i][j]) continue;
-            c++;
-            ret += board[i][j];
+void calculate() {
+    for (vector<pair<int, int>> nations : visited) {
+        int sum = 0;
+        for (pair<int, int> nation : nations) {
+            sum += board[nation.first][nation.second];
         }
-    }
-
-    ret = (ret / c);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (!visited[i][j]) continue;
-            board[i][j] = ret;
+        int avg = sum / nations.size();
+        for (pair<int, int> nation : nations) {
+            board[nation.first][nation.second] = avg;
         }
     }
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> n >> l >> r;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) cin >> board[i][j];
     }
 
-    while (true) {
-        check(0, 0);
-
+    while (check()) {
+        clear();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) cout << visited[i][j] << " ";
-            cout << "\n";
+            for (int j = 0; j < n; j++) {
+                if (flag[i][j]) continue;
+                visited.push_back(vector<pair<int, int>>());
+                go(i, j);
+            }
         }
-        cout << '\n';
-
-        if (!validate()) {
-            break;
-        }
-
-        move();
-        memset(visited, 0, sizeof(visited));
-        cnt++;
+        calculate();
+        if (check()) ret++;
     }
-
-    cout << cnt;
+    cout << ret;
 }
