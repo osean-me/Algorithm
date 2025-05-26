36d90bfa6aff7f67c8738b7ce606f645156b4eba
SeongHeon Sim <deep@osean.me>
2025-05-26 21:43:49 +0900

    add: [3주차] bj4179 복습

--- cpp_algorithm/new_week_3_re/bj4179.cpp
+++ cpp_algorithm/new_week_3_re/bj4179.cpp
@@ -3,45 +3,45 @@
 using namespace std;
 
 string s;
-int r, c;
+int r, c, ret;
 int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
-char board[1000][1000];
-int man_visited[1000][1000], fire_visited[1000][1000];
-queue<pair<int, int>> man_points, fire_points;
+int board[1000][1000];
+vector<vector<int>> man_visited(1000, vector<int>(1000, 0));
+vector<vector<int>> fire_visited(1000, vector<int>(1000, INT_MAX));
+queue<pair<int, int>> points;
 pair<int, int> man;
 
 void go_fire() {
-    while (fire_points.size()) {
+    while (points.size()) {
         int cy, cx;
-        tie(cy, cx) = fire_points.front();
-        fire_points.pop();
+        tie(cy, cx) = points.front();
+        points.pop();
 
         for (int i = 0; i < 4; i++) {
             int ny = cy + dy[i];
             int nx = cx + dx[i];
 
             if (ny < 0 || r <= ny || nx < 0 || c <= nx) continue;
-            if (fire_visited[ny][nx]) continue;
             if (board[ny][nx] == '#') continue;
-
+            if (fire_visited[ny][nx] != INT_MAX) continue;
             fire_visited[ny][nx] = fire_visited[cy][cx] + 1;
-            fire_points.push({ny, nx});
+            points.push({ny, nx});
         }
     }
 }
 
-int go_man() {
+void go_man() {
     man_visited[man.first][man.second] = 1;
-    man_points.push(man);
+    points.push(man);
 
-    while (man_points.size()) {
+    while (points.size()) {
         int cy, cx;
-        tie(cy, cx) = man_points.front();
-        man_points.pop();
+        tie(cy, cx) = points.front();
+        points.pop();
 
-        // 탈출 조건
-        if (cy == 0 || cy == r - 1 || cx == 0 || cx == c - 1) {
-            return man_visited[cy][cx];
+        if (cy == r - 1 || cx == c - 1 || cy == 0 || cx == 0) {
+            ret = man_visited[cy][cx];
+            break;
         }
 
         for (int i = 0; i < 4; i++) {
@@ -49,18 +49,13 @@ int go_man() {
             int nx = cx + dx[i];
 
             if (ny < 0 || r <= ny || nx < 0 || c <= nx) continue;
-            if (man_visited[ny][nx]) continue;
             if (board[ny][nx] == '#') continue;
-            if (fire_visited[ny][nx] &&
-                fire_visited[ny][nx] <= man_visited[cy][cx])
-                continue;
-
+            if (man_visited[ny][nx]) continue;
+            if (fire_visited[ny][nx] <= man_visited[cy][cx] + 1) continue;
             man_visited[ny][nx] = man_visited[cy][cx] + 1;
-            man_points.push({ny, nx});
+            points.push({ny, nx});
         }
     }
-
-    return -1;
 }
 
 int main() {
@@ -73,22 +68,16 @@ int main() {
         cin >> s;
         for (int j = 0; j < c; j++) {
             board[i][j] = s[j];
-
             if (board[i][j] == 'J') man = {i, j};
             if (board[i][j] == 'F') {
-                fire_points.push({i, j});
+                points.push({i, j});
                 fire_visited[i][j] = 1;
             }
         }
     }
 
-    if (man.first == 0 || man.first == r - 1 || man.second == 0 ||
-        man.second == c - 1) {
-        cout << 1;
-    }
-
     go_fire();
-    int ret = go_man();
+    go_man();
 
-    cout << (ret == -1 ? "IMPOSSIBLE" : to_string(ret));
+    cout << (ret ? to_string(ret) : "IMPOSSIBLE");
 }
