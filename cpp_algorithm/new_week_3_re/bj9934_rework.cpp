4a81433b9c6a202f9d898f3666ef7c1586479824
SeongHeon Sim <deep@osean.me>
2025-07-06 17:02:49 +0900

    add: [3주차] bj9934 복습

---
+++ cpp_algorithm/new_week_3_re/bj9934_rework.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int k, n;
+vector<char> nodes;
+vector<vector<char>> tree;
+
+void solution(int idx, int depth) {
+    tree[depth].push_back(nodes[idx]);
+
+    if (depth == 0) {
+        return;
+    }
+
+    int next = depth - 1;
+    int offset = (int)pow(2, next);
+    solution(idx - offset, next);
+    solution(idx + offset, next);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> k;
+    tree.resize(k);
+
+    n = ((int)pow(2, k)) - 1;
+    nodes.resize(n);
+    for (int i = 0; i < n; i++) cin >> nodes[i];
+
+    solution(n / 2, k - 1);
+
+    reverse(tree.begin(), tree.end());
+    for (auto nums : tree) {
+        for (auto num : nums) cout << num << " ";
+        cout << "\n";
+    }
+}
